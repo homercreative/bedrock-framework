7eccdcec47df63047d2a4f587ad34d5b78c698a9
Christopher Thompson <cyberstorm305@gmail.com>
2014-09-04 12:18:22 +0100

    Ignore breakpoints

--- js/bedrock.js
+++ js/bedrock.js
@@ -1,78 +1,110 @@
-$(document).ready(function(){
+jQuery(document).ready(function ($) {
 
-    //Load jQuery library using plain JavaScript
-    (function(){
-        var newscript = document.createElement('script');
-        newscript.type = 'text/javascript';
-        newscript.async = true;
-        newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
-        (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
-    })();
+
 
     // plugins location
 
-    var content = $('.content');
-    var embedlyUrl = "http://cdn.embed.ly/jquery.embedly-3.1.1.js";
+
     var modernizer = "js/plugins/modernizer.js";
     var easing = "js/plugins/jquery.easing.js";
     var slimScroll = "js/plugins/slimscroll.js";
     var owlCarousel = "js/plugins/owl.carousel.min.js";
+    var content = $('.content');
 
     // plugin function call
 
-    getEmbedly(embedlyUrl);
+    var embed = /((http:\/\/(www\.google\.co.uk\/maps.*|maps\.google\.com\/maps\?.*|maps\.google\.com\/\?.*|maps\.google\.com\/maps\/ms\?.*|.*youtube\.com\/watch.*|.*\.youtube\.com\/v\/.*|youtu\.be\/.*|.*\.youtube\.com\/user\/.*|.*\.youtube\.com\/.*#.*\/.*|m\.youtube\.com\/watch.*|m\.youtube\.com\/index.*|.*\.youtube\.com\/profile.*|.*\.youtube\.com\/view_play_list.*|.*\.youtube\.com\/playlist.*|www\.youtube\.com\/embed\/.*))| (https:\/\/(www\.google\.co.uk\/maps.*|maps\.google\.com\/maps\?.*|maps\.google\.com\/\?.*|maps\.google\.com\/maps\/ms\?.*|.*youtube\.com\/watch.*|.*\.youtube\.com\/v\/.*|youtu\.be\/.*|.*\.youtube\.com\/user\/.*|.*\.youtube\.com\/.*#.*\/.*|m\.youtube\.com\/watch.*|m\.youtube\.com\/index.*|.*\.youtube\.com\/profile.*|.*\.youtube\.com\/view_play_list.*|.*\.youtube\.com\/playlist.*|www\.youtube\.com\/embed\/.*)))/i;
+
+    content.embedly({
+        key: 'ff5f84afd87d4a5b898d2ff446735e7b',
+        wmode: 'transparent',
+        method: 'replace',
+        urlRe: embed
+    });
+
+
     getScript(modernizer);
     getScript(easing);
     getScript(slimScroll);
     getOwlCarousel(owlCarousel);
 
+    // == Options
+    // Un comment for full page
+    //getFullpage("js/plugins/jquery.fullPage.js");
+
     // Plugin Functions
 
-    function getScript($url){
+    function getScript($url) {
         $.ajax({
-            url: $url ,
+            url: $url,
             dataType: "script",
             cache: true,
-            error: function(){
+            error: function () {
                 return true;
             },
-            success: function(){
+            success: function () {
 
             }
         });
     }
-    function getEmbedly($url){
+
+    function getOwlCarousel($url) {
         $.ajax({
-            url: $url ,
+            url: $url,
             dataType: "script",
             cache: true,
-            error: function(){
+            error: function () {
                 return true;
             },
-            success: function(){
-                var embed = /((http:\/\/(www\.google\.co.uk\/maps.*|maps\.google\.com\/maps\?.*|maps\.google\.com\/\?.*|maps\.google\.com\/maps\/ms\?.*|.*youtube\.com\/watch.*|.*\.youtube\.com\/v\/.*|youtu\.be\/.*|.*\.youtube\.com\/user\/.*|.*\.youtube\.com\/.*#.*\/.*|m\.youtube\.com\/watch.*|m\.youtube\.com\/index.*|.*\.youtube\.com\/profile.*|.*\.youtube\.com\/view_play_list.*|.*\.youtube\.com\/playlist.*|www\.youtube\.com\/embed\/.*))| (https:\/\/(www\.google\.co.uk\/maps.*|maps\.google\.com\/maps\?.*|maps\.google\.com\/\?.*|maps\.google\.com\/maps\/ms\?.*|.*youtube\.com\/watch.*|.*\.youtube\.com\/v\/.*|youtu\.be\/.*|.*\.youtube\.com\/user\/.*|.*\.youtube\.com\/.*#.*\/.*|m\.youtube\.com\/watch.*|m\.youtube\.com\/index.*|.*\.youtube\.com\/profile.*|.*\.youtube\.com\/view_play_list.*|.*\.youtube\.com\/playlist.*|www\.youtube\.com\/embed\/.*)))/i;
-                $('.content').embedly({
-
-                    key: 'ff5f84afd87d4a5b898d2ff446735e7b'
-
+            success: function () {
+                $("#owl-carousel").owlCarousel({
+                    navigation: true, // Show next and prev buttons
+                    slideSpeed: 300,
+                    paginationSpeed: 400,
+                    singleItem: true
                 });
             }
         });
     }
-    function getOwlCarousel($url){
+
+    function getFullpage($url) {
         $.ajax({
-            url: $url ,
+            url: $url,
             dataType: "script",
             cache: true,
-            error: function(){
+            error: function () {
                 return true;
             },
-            success: function(){
-                $("#owl-demo").owlCarousel({
-                    autoPlay: 3000, //Set AutoPlay to 3 seconds
-                    items : 4,
-                    itemsDesktop : [1199,3],
-                    itemsDesktopSmall : [979,3]
+            success: function () {
+                $('#fullpage').fullpage({
+                    verticalCentered: true,
+                    resize : true,
+                    sectionsColor : ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000'],
+                    anchors:['firstSlide', 'secondSlide'],
+                    scrollingSpeed: 700,
+                    easing: 'easeInQuart',
+                    menu: false,
+                    navigation: true,
+                    navigationPosition: 'right',
+                    navigationTooltips: ['firstSlide', 'secondSlide', 'thirdSlide', 'fourthSlide'],
+                    slidesNavigation: true,
+                    slidesNavPosition: 'bottom',
+                    loopBottom: false,
+                    loopTop: false,
+                    loopHorizontal: true,
+                    autoScrolling: true,
+                    scrollOverflow: false,
+                    css3: false,
+                    paddingTop: '3em',
+                    paddingBottom: '10px',
+                    normalScrollElements: '#element1, .element2',
+                    normalScrollElementTouchThreshold: 5,
+                    keyboardScrolling: true,
+                    touchSensitivity: 15,
+                    continuousVertical: false,
+                    animateAnchor: true,
+                    sectionSelector: '.section',
+                    slideSelector: '.slide'
                 });
             }
         });
